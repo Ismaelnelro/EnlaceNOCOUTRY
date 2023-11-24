3409119af9bbc457028165ba21e9d04981a5034e
IsmaelRo <ismaelro@localhost.localdomain>
2023-11-24 16:37:14 -0300

    Se agrego ConditionPage.tsx con estilos responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Layout from "./layout/Layout"
 import { AboutPage } from "./pages/AboutPage"
+import { ConditionPage } from "./pages/ConditionPage"
 
 function App() {
 
   return (
     <>
       {/* <HomePage/> */}
-      <AboutPage/>
+      {/* <AboutPage/> */}
+      <ConditionPage/>
     </>
   )
 }
